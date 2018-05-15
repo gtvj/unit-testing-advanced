10fd4de88070e313ae18ca63101aae2c7a20d516
Gwyn Jones <m@gwnjns.com>
2018-05-15 10:54:28 +0100

    Add simple unit test

--- test/simple-spec.js
+++ test/simple-spec.js
@@ -1,3 +1,12 @@
-describe("adder()", function () {
-    it("should do a simple sum");
+let { expect } = require('chai');
+let calculator = require('../lib/calculator');
+
+describe("add()", function () {
+    it("should do a simple sum", function () {
+
+        let result = calculator.add(2, 2);
+
+        expect(result).to.equal(4);
+
+    });
 });
