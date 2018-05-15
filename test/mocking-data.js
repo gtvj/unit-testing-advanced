5c0bce20d9bc9b2a5c526b82ee6eef6c5159990f
Gwyn Jones <m@gwnjns.com>
2018-05-15 14:34:59 +0100

    Add illustration of mocking a data store
    In this example we use the 'rewire' module to allow us to replace
    properties of our SUT

---
+++ test/mocking-data.js
@@ -0,0 +1,34 @@
+let rewire = require('rewire');
+let newsletter_app = rewire('../lib/newsletter_application');
+let assert = require('assert');
+
+
+describe("Checking for users", function () {
+
+    beforeEach(function () {
+        this.test_data = [
+            {
+                name: 'Bob',
+                email: 'bob@bob.com'
+            },
+            {
+                name: 'Jen',
+                email: 'jen@jen.com'
+            }
+        ];
+
+        newsletter_app.__set__('data', this.test_data);
+    });
+
+    it("Should check for users in the data", function () {
+
+        let user = 'bob@bob.com'
+
+        let result = newsletter_app.check_for_user(user);
+
+        assert.ok(result, "the user was not found");
+
+    });
+});
+
+
